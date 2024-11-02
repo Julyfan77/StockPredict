2fa32a083a13d38ae2590ab4d7c90e9c24032418
lyujt <lvjt@bu.edu>
2024-11-02 17:15:30 -0400

    add MAPE measure, finetune epoch

--- Day_to_day/data_preprocess.py
+++ Day_to_day/data_preprocess.py
@@ -38,3 +38,30 @@ def getdata(path):
     X = X.reshape((X.shape[0], X.shape[1], 1))  # 每个时间步一个特征
 
     return X,y,scaler
+# def getdata(path):
+#     data = pd.read_csv(path, index_col='Date', parse_dates=True)
+#
+#     # Select 'Close' and split data into train and test sets
+#     close_prices = data[['Close']].values  # Reshape required for scaler
+#     train_size = int(len(close_prices) * 0.8)
+#
+#     # Fit scaler only on the training data
+#     scaler = MinMaxScaler(feature_range=(0, 1))
+#     close_prices_train = close_prices[:train_size]
+#     close_prices_test = close_prices[train_size:]
+#
+#     # Scale training and test data
+#     close_prices_train_scaled = scaler.fit_transform(close_prices_train)
+#     close_prices_test_scaled = scaler.transform(close_prices_test)
+#
+#     # Prepare sequences for training and test data
+#     sequence_length = 30
+#     X_train, y_train = create_sequences(close_prices_train_scaled, sequence_length)
+#     X_test, y_test = create_sequences(close_prices_test_scaled, sequence_length)
+#
+#     # Reshape inputs to [samples, timesteps, features]
+#     X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
+#     X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+#
+#     return X_train, y_train, X_test, y_test, scaler
+
